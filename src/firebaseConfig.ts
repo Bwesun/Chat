44f6bddd4b8700ab0517ee24e8e4a5758902b78a
Bwesun <maturinnocent@gmail.com>
2025-05-22 21:46:27 +0100

    fixes and setups

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection} from 'firebase/firestore'
+
+
+// Firebase Config
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET, 
+    messagingSenderId: import.meta.env.VITE_MESSAGESENDERID,
+    appId: import.meta.env.VITE_APPID
+};
+
+// Initialize Firebase
+const appinit = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Auth
+const db = getFirestore(appinit);
+const auth = getAuth()
+
+// Collection reference
+const colRef = collection(db, "users");
+const transRef = collection(db, "transactions");
+const orgRef = collection(db, "organizations");
+const payRef = collection(db, "payments");
+
+export { appinit, auth, db, colRef, transRef, payRef, orgRef };
