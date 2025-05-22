2289191c3927932d67fef0edf4c24790bbdbced2
Bwesun <maturinnocent@gmail.com>
2025-05-22 16:59:43 +0100

    first commit - created and installed dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.censonochat',
+  appName: 'censonochat',
+  webDir: 'dist'
+};
+
+export default config;
