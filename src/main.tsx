276f5e3ed9b54b1f8379f2ba5e0902d7a33d338a
Bwesun <maturinnocent@gmail.com>
2025-05-29 18:26:41 +0100

    feat: add @ionic/pwa-elements dependency and initialize custom elements in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import {defineCustomElements} from '@ionic/pwa-elements/loader';
+
+// Call PWA Element for camera launch on web
+defineCustomElements(window);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
